87adfef277d938fb8a7d49ff928b0b267af85c7c
Logan Born <lcborn4@gmail.com>
2022-03-27 21:12:27 -0400

    styling the confirmation required page

--- src/app/modules/auth/confirmation-required/confirmation-required.component.ts
+++ src/app/modules/auth/confirmation-required/confirmation-required.component.ts
@@ -2,17 +2,16 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { fuseAnimations } from '@fuse/animations';
 
 @Component({
-    selector     : 'auth-confirmation-required',
-    templateUrl  : './confirmation-required.component.html',
+    selector: 'auth-confirmation-required',
+    templateUrl: './confirmation-required.component.html',
     encapsulation: ViewEncapsulation.None,
-    animations   : fuseAnimations
+    animations: fuseAnimations,
 })
-export class AuthConfirmationRequiredComponent
-{
+export class AuthConfirmationRequiredComponent {
+    fullImagePath = '../assets/images/logo/logo_2020_9.svg';
+
     /**
      * Constructor
      */
-    constructor()
-    {
-    }
+    constructor() {}
 }
