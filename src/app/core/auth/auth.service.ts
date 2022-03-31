0641f8b5ef12ee1309f78893dc3d4d6b2ac2cdeb
Logan Born <lcborn4@gmail.com>
2022-03-31 15:26:28 -0400

    eslint disable

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/member-ordering */
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError, Observable, of, switchMap, throwError } from 'rxjs';
