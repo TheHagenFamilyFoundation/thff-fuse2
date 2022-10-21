168bb2f61825372be6ebb3a917c3320d3cf8da31
Logan Born <lcborn4@gmail.com>
2022-10-21 16:04:46 -0400

    route to welcome

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -16,7 +16,7 @@ export const appRoutes: Route[] = [
     // After the user signs in, the sign in page will redirect the user to the 'signed-in-redirect'
     // path. Below is another redirection for that path to redirect the user to the desired
     // location. This is a small convenience to keep all main routes together here on this file.
-    { path: 'signed-in-redirect', pathMatch: 'full', redirectTo: 'example' },
+    { path: 'signed-in-redirect', pathMatch: 'full', redirectTo: 'welcome' },
 
     // Auth routes for guests
     {
@@ -136,7 +136,13 @@ export const appRoutes: Route[] = [
                         (m) => m.ExampleModule
                     ),
             },
-
+            {
+                path: 'welcome',
+                loadChildren: () =>
+                    import('app/modules/admin/welcome/welcome.module').then(
+                        (m) => m.WelcomeModule
+                    ),
+            },
             // Pages
             {
                 path: 'pages',
