9c0afeff4cf9481a54111aadd219b7cd02e5963b
Logan Born <lcborn4@gmail.com>
2022-11-08 00:10:12 -0500

    formatting for auth and check

--- src/app/core/auth/guards/auth.guard.ts
+++ src/app/core/auth/guards/auth.guard.ts
@@ -1,22 +1,26 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import {
+    ActivatedRouteSnapshot,
+    CanActivate,
+    CanActivateChild,
+    CanLoad,
+    Route,
+    Router,
+    RouterStateSnapshot,
+    UrlSegment,
+    UrlTree,
+} from '@angular/router';
 import { Observable, of, switchMap } from 'rxjs';
 import { AuthService } from 'app/core/auth/auth.service';
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: 'root',
 })
-export class AuthGuard implements CanActivate, CanActivateChild, CanLoad
-{
+export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
     /**
      * Constructor
      */
-    constructor(
-        private _authService: AuthService,
-        private _router: Router
-    )
-    {
-    }
+    constructor(private _authService: AuthService, private _router: Router) {}
 
     // -----------------------------------------------------------------------------------------------------
     // @ Public methods
@@ -28,8 +32,10 @@ export class AuthGuard implements CanActivate, CanActivateChild, CanLoad
      * @param route
      * @param state
      */
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean
-    {
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ): Observable<boolean> | Promise<boolean> | boolean {
         const redirectUrl = state.url === '/sign-out' ? '/' : state.url;
         return this._check(redirectUrl);
     }
@@ -40,8 +46,14 @@ export class AuthGuard implements CanActivate, CanActivateChild, CanLoad
      * @param childRoute
      * @param state
      */
-    canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree
-    {
+    canActivateChild(
+        childRoute: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ):
+        | Observable<boolean | UrlTree>
+        | Promise<boolean | UrlTree>
+        | boolean
+        | UrlTree {
         const redirectUrl = state.url === '/sign-out' ? '/' : state.url;
         return this._check(redirectUrl);
     }
@@ -52,8 +64,10 @@ export class AuthGuard implements CanActivate, CanActivateChild, CanLoad
      * @param route
      * @param segments
      */
-    canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean
-    {
+    canLoad(
+        route: Route,
+        segments: UrlSegment[]
+    ): Observable<boolean> | Promise<boolean> | boolean {
         return this._check('/');
     }
 
@@ -67,26 +81,26 @@ export class AuthGuard implements CanActivate, CanActivateChild, CanLoad
      * @param redirectURL
      * @private
      */
-    private _check(redirectURL: string): Observable<boolean>
-    {
+    private _check(redirectURL: string): Observable<boolean> {
         // Check the authentication status
-        return this._authService.check()
-                   .pipe(
-                       switchMap((authenticated) => {
+        return this._authService.check().pipe(
+            switchMap((authenticated) => {
+                console.log('checking if user has access');
 
-                           // If the user is not authenticated...
-                           if ( !authenticated )
-                           {
-                               // Redirect to the sign-in page
-                               this._router.navigate(['sign-in'], {queryParams: {redirectURL}});
+                // If the user is not authenticated...
+                if (!authenticated) {
+                    // Redirect to the sign-in page
+                    this._router.navigate(['sign-in'], {
+                        queryParams: { redirectURL },
+                    });
 
-                               // Prevent the access
-                               return of(false);
-                           }
+                    // Prevent the access
+                    return of(false);
+                }
 
-                           // Allow the access
-                           return of(true);
-                       })
-                   );
+                // Allow the access
+                return of(true);
+            })
+        );
     }
 }
