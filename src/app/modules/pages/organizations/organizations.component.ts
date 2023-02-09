d216e84e4608faa3c534d6d39717f115353b414b
Logan Born <lcborn4@gmail.com>
2023-02-09 18:43:13 -0500

    navigate to create organization page

--- src/app/modules/pages/organizations/organizations.component.ts
+++ src/app/modules/pages/organizations/organizations.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
     selector: 'app-organizations',
     templateUrl: './organizations.component.html',
     styleUrls: ['./organizations.component.scss'],
 })
 export class OrganizationsComponent implements OnInit {
-    constructor() {}
+    constructor(private router: Router) {}
     ngOnInit(): void {}
-    createOrganization(): void {}
+    createOrganization(): void {
+        console.log('create organization');
+        this.router.navigate(['/pages/organization/create']);
+    }
 }
