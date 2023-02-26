c09b52157cef15bb54d2ff6b6890e92767b46f1f
Logan Born <lcborn4@gmail.com>
2023-02-26 13:44:59 -0500

    initialize backend url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 
+import { AuthService } from './core/auth/auth.service';
+
 @Component({
-    selector   : 'app-root',
+    selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls  : ['./app.component.scss']
+    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent
-{
+export class AppComponent {
     /**
      * Constructor
      */
-    constructor()
-    {
+    constructor(public authService: AuthService) {
+        console.log('app-initialize backendurl');
+        this.authService.initializeBackendURL();
     }
 }
